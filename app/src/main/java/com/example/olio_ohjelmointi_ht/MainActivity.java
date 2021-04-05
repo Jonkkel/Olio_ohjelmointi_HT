4683996052f696acfa21ae975112c3034bca3260
jonkkel <79365635+jonkkel@users.noreply.github.com>
2021-04-05 17:11:25 +0300

    Initial commit

---
+++ app/src/main/java/com/example/olio_ohjelmointi_ht/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.olio_ohjelmointi_ht;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
