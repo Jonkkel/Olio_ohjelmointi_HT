ad6b8af8fc9abf919572f6f60242610f49f0759d
sarajessicv <81183145+sarajessicv@users.noreply.github.com>
2021-04-16 13:58:40 +0300

    näkyykö

---
+++ app/src/main/java/com/example/olio_ohjelmointi_ht/LogInTool.java
@@ -0,0 +1,4 @@
+package com.example.olio_ohjelmointi_ht;
+
+public class LogInTool {
+}
---
+++ app/src/main/java/com/example/olio_ohjelmointi_ht/User.java
@@ -0,0 +1,38 @@
+package com.example.olio_ohjelmointi_ht;
+
+public class User {
+    String name;
+    int age;
+    String password;
+    String username;
+    String email;
+    String city;
+
+    public User(){
+
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
