090039cbba22c8ffc088f3fd3f21819e58a09967
sarajessicv <81183145+sarajessicv@users.noreply.github.com>
2021-04-28 20:51:06 +0300

    sekasin

--- app/src/main/java/com/example/olio_ohjelmointi_ht/Begin_progress_screen.java
+++ app/src/main/java/com/example/olio_ohjelmointi_ht/Begin_progress_screen.java
@@ -20,7 +20,7 @@ import com.jjoe64.graphview.GraphView;
 
 import static android.content.Context.MODE_PRIVATE;
 
-public class Begin_progress_screen extends Fragment implements View.OnClickListener{
+public class Begin_progress_screen extends Fragment implements View.OnClickListener {
 
     EditText exercise;
     EditText weight;
@@ -43,39 +43,38 @@ public class Begin_progress_screen extends Fragment implements View.OnClickListe
         weight_graph = (GraphView) v.findViewById(R.id.weight_graph);
 
         context = getContext();
-
+        exercise_button.setOnClickListener(this);
+        weight_button.setOnClickListener(this);
 
         return inflater.inflate(R.layout.fragment_progress_screen, container, false);
     }
-/* Voi käyttää ja kannattaa käyttää, jos on tekstikentää jonka tekstiä haluaa muokata.
-    Ajetaan sen jälkeen kun fragmentti on luotu - Turvallisempi metodi kuin ylempi*/
+
+    /* Voi käyttää ja kannattaa käyttää, jos on tekstikentää jonka tekstiä haluaa muokata.
+        Ajetaan sen jälkeen kun fragmentti on luotu - Turvallisempi metodi kuin ylempi*/
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
 
-
-        exercise_button.setOnClickListener(this);
-        weight_button.setOnClickListener(this);
         super.onViewCreated(view, savedInstanceState);
     }
 
     @Override
     public void onClick(View v) {
+        HarryPlotter plotter = HarryPlotter.getInstance();
+        PathFinder path = PathFinder.getInstance(this.context);
         switch (v.getId()) {
             case R.id.submit_exercise:
                 exercise_amount = exercise.getText().toString();
+                System.out.println("testi");
+                UDP.writeCSV(path.pathBuilder() + "Exercise_data.csv", Double.parseDouble(exercise_amount));
+                plotter.readCSV(path.pathBuilder() + "Exercise_data.csv", exercise_graph);
                 System.out.println(exercise_amount);
                 break;
             case R.id.submit_weight:
                 weight_amount = weight.getText().toString();
-
-
+                UDP.writeCSV(path.pathBuilder() + "Weight_data.csv", Double.parseDouble(weight_amount));
+                plotter.readCSV(path.pathBuilder() + "Weight_data.csv", weight_graph);
+                System.out.println(exercise_amount);
                 break;
-        UDP = User_data_plotting.getInstance();
-        HarryPlotter plotter = HarryPlotter.getInstance();
-        SharedPreferences prefs = context.getSharedPreferences("User", MODE_PRIVATE);
-        String cUser = prefs.getString("Current User", "");
-
-        UDP.writeCSV("Exercise_data_" + cUser , Double.parseDouble(exercise_amount));
-        plotter.readCSV("Exercise_data_" + cUser , exercise_graph);
+        }
     }
 }
